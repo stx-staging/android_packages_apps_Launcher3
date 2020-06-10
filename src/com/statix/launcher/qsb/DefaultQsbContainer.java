AOSPA/android_packages_apps_ParanoidLauncher

ParanoidLauncher: Add support for Google QuickSearchBox
Moves QSB to the hotseat and adds support for basic Google search functionality. This iteration does not support extended features (ex. Google Doodle) but has plans to support these features later on

Change-Id: Ia905b475e133c82814745e0cd037d07f7b41d1a9

src/com/paranoid/launcher/qsb/DefaultQsbContainer.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2019 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.paranoid.launcher.qsb;
+
+import static com.android.launcher3.LauncherState.ALL_APPS;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.android.launcher3.ExtendedEditText;
+import com.android.launcher3.Launcher;
+import com.android.launcher3.allapps.AllAppsContainerView;
+import com.android.launcher3.allapps.AllAppsStore.OnUpdateListener;
+import com.android.launcher3.allapps.AlphabeticalAppsList;
+import com.android.launcher3.allapps.search.AllAppsSearchBarController;
+import com.android.launcher3.allapps.search.AllAppsSearchBarController.Callbacks;
+import com.android.launcher3.util.ComponentKey;
+
+import java.util.ArrayList;
+
+public class DefaultQsbContainer extends ExtendedEditText implements OnUpdateListener, Callbacks {
+
+    public AllAppsSearchBarController mController;
+    public AllAppsQsbContainer mAllAppsQsb;
+    public AlphabeticalAppsList mApps;
+    public AllAppsContainerView mAppsView;
+
+    public DefaultQsbContainer(Context context, AttributeSet attributeSet) {
+        this(context, attributeSet, 0);
+    }
+
+    public DefaultQsbContainer(Context context, AttributeSet attributeSet, int i) {
+        super(context, attributeSet, i);
+        mController = new AllAppsSearchBarController();
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Launcher.getLauncher(getContext()).getAppsView().getAppsStore().addUpdateListener(this);
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        Launcher.getLauncher(getContext()).getAppsView().getAppsStore().removeUpdateListener(this);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        Launcher.getLauncher(getContext()).getStateManager().goToState(ALL_APPS);
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    public void onSearchResult(String query, ArrayList<ComponentKey> apps) {
+        if (apps != null && getParent() != null) {
+            mApps.setOrderedFilter(apps);
+            refreshSearchResult();
+            mAppsView.setLastSearchQuery(query);
+        }
+    }
+
+    @Override
+    public void clearSearchResult() {
+        if (getParent() != null) {
+            if (mApps.setOrderedFilter(null)) {
+                refreshSearchResult();
+            }
+            mAllAppsQsb.setKeepDefaultView(true);
+            mAppsView.onClearSearchResult();
+            mAllAppsQsb.setKeepDefaultView(false);
+        }
+    }
+
+    @Override
+    public void onAppsUpdated() {
+        mController.refreshSearchResult();
+    }
+
+    public void refreshSearchResult() {
+        mAllAppsQsb.updateAlpha(0);
+        mAppsView.onSearchResultsChanged();
+    }
+}

src/com/paranoid/launcher/qsb/configs/ItemDragListener.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2019 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.paranoid.launcher.qsb.configs;
+
+import android.content.pm.LauncherActivityInfo;
+import android.graphics.Rect;
+import android.view.View;
+
+import com.android.launcher3.ItemInfo;
+import com.android.launcher3.InstallShortcutReceiver;
+import com.android.launcher3.WorkspaceItemInfo;
+import com.android.launcher3.compat.ShortcutConfigActivityInfo.ShortcutConfigActivityInfoVO;
+import com.android.launcher3.dragndrop.BaseItemDragListener;
+import com.android.launcher3.userevent.nano.LauncherLogProto.Target;
+import com.android.launcher3.widget.PendingAddShortcutInfo;
+import com.android.launcher3.widget.PendingItemDragHelper;
+
+public class ItemDragListener extends BaseItemDragListener {
+
+    public LauncherActivityInfo mActivityInfo;
+
+    public ItemDragListener(LauncherActivityInfo activityInfo, Rect rect) {
+        super(rect, rect.width(), rect.width());
+        mActivityInfo = activityInfo;
+    }
+
+    public PendingItemDragHelper createDragHelper() {
+        PendingAddShortcutInfo tag = new PendingAddShortcutInfo(new ShortcutConfigActivityInfoVO(mActivityInfo) {
+            public WorkspaceItemInfo createShortcutInfo() {
+                return InstallShortcutReceiver.fromActivityInfo(mActivityInfo, mLauncher);
+            }
+        });
+        View view = new View(mLauncher);
+        view.setTag(tag);
+        return new PendingItemDragHelper(view);
+    }
+  
+    @Override
+    public void fillInLogContainerData(View v, ItemInfo info, Target target, Target targetParent) {
+    }
+}

src/com/paranoid/launcher/qsb/configs/LongClickReceiver.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2019 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.paranoid.launcher.qsb.configs;
+
+import android.content.BroadcastReceiver;
+import android.content.ClipData;
+import android.content.ClipData.Item;
+import android.content.ClipDescription;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.LauncherActivityInfo;
+import android.os.Bundle;
+
+import com.paranoid.launcher.ParanoidLauncher;
+import com.paranoid.launcher.search.AppSearchProvider;
+
+import com.android.launcher3.compat.LauncherAppsCompat;
+import com.android.launcher3.util.ComponentKey;
+
+import java.lang.ref.WeakReference;
+
+public class LongClickReceiver extends BroadcastReceiver {
+
+    public static WeakReference<ParanoidLauncher> mWeakReference = new WeakReference(null);
+
+    public static void getWeakReference(ParanoidLauncher launcher) {
+        mWeakReference = new WeakReference(launcher);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ParanoidLauncher launcher = (ParanoidLauncher) mWeakReference.get();
+        if (launcher != null) {
+            ComponentKey searchUri = AppSearchProvider.uriToComponent(intent.getData(), context);
+            LauncherActivityInfo resolveActivity = LauncherAppsCompat.getInstance(context)
+                .resolveActivity(new Intent("android.intent.action.MAIN")
+                .setComponent(searchUri.componentName), searchUri.user);
+            if (resolveActivity != null) {
+                ItemDragListener onDragListener = new ItemDragListener(resolveActivity, intent.getSourceBounds());
+                onDragListener.init(launcher, false);
+                launcher.getDragLayer().setOnDragListener(onDragListener);
+                ClipData clipData = new ClipData(new ClipDescription("", new String[]{onDragListener.getMimeType()}), new Item(""));
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("clip_data", clipData);
+                setResult(-1, null, bundle);
+            }
+        }
+    }
+}

src/com/paranoid/launcher/search/SearchHandler.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2019 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.paranoid.launcher.search;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.net.Uri.Builder;
+import android.os.Handler;
+import android.os.Handler.Callback;
+import android.os.HandlerThread;
+import android.os.Message;
+
+import com.android.launcher3.allapps.search.AllAppsSearchBarController.Callbacks;
+import com.android.launcher3.allapps.search.SearchAlgorithm;
+
+public class SearchHandler implements SearchAlgorithm, Callback {
+
+    public static HandlerThread handlerThread;
+    public Context mContext;
+    public Handler mHandler;
+    public boolean mInterruptActiveRequests;
+    public Handler mUiHandler = new Handler(this);
+
+    public SearchHandler(Context context) {
+        mContext = context;
+        if (handlerThread == null) {
+            handlerThread = new HandlerThread("search-thread", -2);
+            handlerThread.start();
+        }
+        mHandler = new Handler(handlerThread.getLooper(), this);
+    }
+
+    public void queryResult(SearchResult componentList) {
+        Cursor cursor = null;
+        try {
+            cursor = mContext.getContentResolver().query(new Builder()
+                .scheme("content")
+                .authority("com.paranoid.launcher.appssearch")
+                .appendPath(componentList.mQuery)
+                .build(), null, null, null, null);
+            int suggestIntentData = cursor.getColumnIndex("suggest_intent_data");
+            while (cursor.moveToNext()) {
+                componentList.mApps.add(AppSearchProvider.uriToComponent(Uri.parse(cursor.getString(suggestIntentData)), mContext));
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        Message.obtain(mUiHandler, 200, componentList).sendToTarget();
+    }
+
+    public void cancel(boolean interruptActiveRequests) {
+        mInterruptActiveRequests = interruptActiveRequests;
+        mHandler.removeMessages(100);
+        if (interruptActiveRequests) {
+            mUiHandler.removeMessages(200);
+        }
+    }
+
+    public void doSearch(String query, Callbacks callback) {
+        mHandler.removeMessages(100);
+        Message.obtain(mHandler, 100, new SearchResult(query, callback)).sendToTarget();
+    }
+
+    public boolean handleMessage(Message message) {
+        int i = message.what;
+        if (i == 100) {
+            queryResult((SearchResult) message.obj);
+        } else if (i != 200) {
+            return false;
+        } else {
+            if (!mInterruptActiveRequests) {
+                SearchResult result = (SearchResult) message.obj;
+                result.mCallbacks.onSearchResult(result.mQuery, result.mApps);
+            }
+        }
+        return true;
+    }
+}

src/com/paranoid/launcher/search/SearchResult.java
@@ -13,18 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.paranoid.launcher;
+package com.paranoid.launcher.search;
 
-import com.android.launcher3.Launcher;
-import com.android.launcher3.LauncherCallbacks;
+import com.android.launcher3.allapps.search.AllAppsSearchBarController.Callbacks;
+import com.android.launcher3.util.ComponentKey;
 
-public class ParanoidLauncher extends Launcher {
+import java.util.ArrayList;
 
-    public ParanoidLauncher() {
-        setLauncherCallbacks(new ParanoidLauncherCallbacks(this));
-    }
+public class SearchResult {
 
-    public LauncherCallbacks getLauncherCallbacks() {
-        return mLauncherCallbacks;
+    public final ArrayList<ComponentKey> mApps = new ArrayList();
+    public final Callbacks mCallbacks;
+    public final String mQuery;
+
+    public SearchResult(String query, Callbacks callbacks) {
+        mQuery = query;
+        mCallbacks = callbacks;
     }
 }

stx-staging/android_packages_apps_Launcher3

src/com/statix/launcher/qsb/DefaultQsbContainer.java
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2019 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.statix.launcher.qsb;
+
+import static com.android.launcher3.LauncherState.ALL_APPS;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.android.launcher3.ExtendedEditText;
+import com.android.launcher3.Launcher;
+import com.android.launcher3.allapps.AllAppsContainerView;
+import com.android.launcher3.allapps.AllAppsStore.OnUpdateListener;
+import com.android.launcher3.allapps.AlphabeticalAppsList;
+import com.android.launcher3.allapps.search.AllAppsSearchBarController;
+import com.android.launcher3.allapps.search.AllAppsSearchBarController.Callbacks;
+import com.android.launcher3.util.ComponentKey;
+
+import java.util.ArrayList;
+
+public class DefaultQsbContainer extends ExtendedEditText implements OnUpdateListener, Callbacks {
+
+    public AllAppsSearchBarController mController;
+    public AllAppsQsbContainer mAllAppsQsb;
+    public AlphabeticalAppsList mApps;
+    public AllAppsContainerView mAppsView;
+
+    public DefaultQsbContainer(Context context, AttributeSet attributeSet) {
+        this(context, attributeSet, 0);
+    }
+
+    public DefaultQsbContainer(Context context, AttributeSet attributeSet, int i) {
+        super(context, attributeSet, i);
+        mController = new AllAppsSearchBarController();
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Launcher.getLauncher(getContext()).getAppsView().getAppsStore().addUpdateListener(this);
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        Launcher.getLauncher(getContext()).getAppsView().getAppsStore().removeUpdateListener(this);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        Launcher.getLauncher(getContext()).getStateManager().goToState(ALL_APPS);
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    public void onSearchResult(String query, ArrayList<ComponentKey> apps) {
+        if (apps != null && getParent() != null) {
+            mApps.setOrderedFilter(apps);
+            refreshSearchResult();
+            mAppsView.setLastSearchQuery(query);
+        }
+    }
+
+    @Override
+    public void clearSearchResult() {
+        if (getParent() != null) {
+            if (mApps.setOrderedFilter(null)) {
+                refreshSearchResult();
+            }
+            mAllAppsQsb.setKeepDefaultView(true);
+            mAppsView.onClearSearchResult();
+            mAllAppsQsb.setKeepDefaultView(false);
+        }
+    }
+
+    @Override
+    public void onAppsUpdated() {
+        mController.refreshSearchResult();
+    }
+
+    public void refreshSearchResult() {
+        mAllAppsQsb.updateAlpha(0);
+        mAppsView.onSearchResultsChanged();
+    }
+}

src/com/statix/launcher/qsb/configs/ItemDragListener.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2019 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.statix.launcher.qsb.configs;
+
+import android.content.pm.LauncherActivityInfo;
+import android.graphics.Rect;
+import android.view.View;
+
+import com.android.launcher3.ItemInfo;
+import com.android.launcher3.InstallShortcutReceiver;
+import com.android.launcher3.WorkspaceItemInfo;
+import com.android.launcher3.compat.ShortcutConfigActivityInfo.ShortcutConfigActivityInfoVO;
+import com.android.launcher3.dragndrop.BaseItemDragListener;
+import com.android.launcher3.userevent.nano.LauncherLogProto.Target;
+import com.android.launcher3.widget.PendingAddShortcutInfo;
+import com.android.launcher3.widget.PendingItemDragHelper;
+
+public class ItemDragListener extends BaseItemDragListener {
+
+    public LauncherActivityInfo mActivityInfo;
+
+    public ItemDragListener(LauncherActivityInfo activityInfo, Rect rect) {
+        super(rect, rect.width(), rect.width());
+        mActivityInfo = activityInfo;
+    }
+
+    public PendingItemDragHelper createDragHelper() {
+        PendingAddShortcutInfo tag = new PendingAddShortcutInfo(new ShortcutConfigActivityInfoVO(mActivityInfo) {
+            public WorkspaceItemInfo createShortcutInfo() {
+                return InstallShortcutReceiver.fromActivityInfo(mActivityInfo, mLauncher);
+            }
+        });
+        View view = new View(mLauncher);
+        view.setTag(tag);
+        return new PendingItemDragHelper(view);
+    }
+  
+    @Override
+    public void fillInLogContainerData(View v, ItemInfo info, Target target, Target targetParent) {
+    }
+}

src/com/statix/launcher/qsb/configs/LongClickReceiver.java
@@ -1,0 +1,63 @@
+/*
+ * Copyright (C) 2019 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.statix.launcher.qsb.configs;
+
+import android.content.BroadcastReceiver;
+import android.content.ClipData;
+import android.content.ClipData.Item;
+import android.content.ClipDescription;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.LauncherActivityInfo;
+import android.os.Bundle;
+
+import com.statix.launcher.CustomLauncher;
+import com.statix.launcher.search.AppSearchProvider;
+
+import com.android.launcher3.compat.LauncherAppsCompat;
+import com.android.launcher3.util.ComponentKey;
+
+import java.lang.ref.WeakReference;
+
+public class LongClickReceiver extends BroadcastReceiver {
+
+    public static WeakReference<CustomLauncher> mWeakReference = new WeakReference(null);
+
+    public static void getWeakReference(CustomLauncher launcher) {
+        mWeakReference = new WeakReference(launcher);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        CustomLauncher launcher = (CustomLauncher) mWeakReference.get();
+        if (launcher != null) {
+            ComponentKey searchUri = AppSearchProvider.uriToComponent(intent.getData(), context);
+            LauncherActivityInfo resolveActivity = LauncherAppsCompat.getInstance(context)
+                .resolveActivity(new Intent("android.intent.action.MAIN")
+                .setComponent(searchUri.componentName), searchUri.user);
+            if (resolveActivity != null) {
+                ItemDragListener onDragListener = new ItemDragListener(resolveActivity, intent.getSourceBounds());
+                onDragListener.init(launcher, false);
+                launcher.getDragLayer().setOnDragListener(onDragListener);
+                ClipData clipData = new ClipData(new ClipDescription("", new String[]{onDragListener.getMimeType()}), new Item(""));
+                Bundle bundle = new Bundle();
+                bundle.putParcelable("clip_data", clipData);
+                setResult(-1, null, bundle);
+            }
+        }
+    }
+}

src/com/statix/launcher/search/SearchHandler.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2019 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.statix.launcher.search;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.net.Uri.Builder;
+import android.os.Handler;
+import android.os.Handler.Callback;
+import android.os.HandlerThread;
+import android.os.Message;
+
+import com.android.launcher3.allapps.search.AllAppsSearchBarController.Callbacks;
+import com.android.launcher3.allapps.search.SearchAlgorithm;
+
+public class SearchHandler implements SearchAlgorithm, Callback {
+
+    public static HandlerThread handlerThread;
+    public Context mContext;
+    public Handler mHandler;
+    public boolean mInterruptActiveRequests;
+    public Handler mUiHandler = new Handler(this);
+
+    public SearchHandler(Context context) {
+        mContext = context;
+        if (handlerThread == null) {
+            handlerThread = new HandlerThread("search-thread", -2);
+            handlerThread.start();
+        }
+        mHandler = new Handler(handlerThread.getLooper(), this);
+    }
+
+    public void queryResult(SearchResult componentList) {
+        Cursor cursor = null;
+        try {
+            cursor = mContext.getContentResolver().query(new Builder()
+                .scheme("content")
+                .authority("com.statix.launcher.appssearch")
+                .appendPath(componentList.mQuery)
+                .build(), null, null, null, null);
+            int suggestIntentData = cursor.getColumnIndex("suggest_intent_data");
+            while (cursor.moveToNext()) {
+                componentList.mApps.add(AppSearchProvider.uriToComponent(Uri.parse(cursor.getString(suggestIntentData)), mContext));
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        Message.obtain(mUiHandler, 200, componentList).sendToTarget();
+    }
+
+    public void cancel(boolean interruptActiveRequests) {
+        mInterruptActiveRequests = interruptActiveRequests;
+        mHandler.removeMessages(100);
+        if (interruptActiveRequests) {
+            mUiHandler.removeMessages(200);
+        }
+    }
+
+    public void doSearch(String query, Callbacks callback) {
+        mHandler.removeMessages(100);
+        Message.obtain(mHandler, 100, new SearchResult(query, callback)).sendToTarget();
+    }
+
+    public boolean handleMessage(Message message) {
+        int i = message.what;
+        if (i == 100) {
+            queryResult((SearchResult) message.obj);
+        } else if (i != 200) {
+            return false;
+        } else {
+            if (!mInterruptActiveRequests) {
+                SearchResult result = (SearchResult) message.obj;
+                result.mCallbacks.onSearchResult(result.mQuery, result.mApps);
+            }
+        }
+        return true;
+    }
+}

src/com/statix/launcher/search/SearchResult.java
@@ -13,18 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.statix.launcher;
+package com.statix.launcher.search;
 
-import com.android.launcher3.Launcher;
-import com.android.launcher3.LauncherCallbacks;
+import com.android.launcher3.allapps.search.AllAppsSearchBarController.Callbacks;
+import com.android.launcher3.util.ComponentKey;
 
-public class CustomLauncher extends Launcher {
+import java.util.ArrayList;
 
-    public CustomLauncher() {
-        setLauncherCallbacks(new CustomLauncherCallbacks(this));
-    }
+public class SearchResult {
 
-    public LauncherCallbacks getLauncherCallbacks() {
-        return mLauncherCallbacks;
+    public final ArrayList<ComponentKey> mApps = new ArrayList();
+    public final Callbacks mCallbacks;
+    public final String mQuery;
+
+    public SearchResult(String query, Callbacks callbacks) {
+        mQuery = query;
+        mCallbacks = callbacks;
     }
 }